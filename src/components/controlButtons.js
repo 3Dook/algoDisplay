294906e6913ad22c796c94921305e0b307efb7e8
doandu <doandu@oregonstate.edu>
2022-03-28 22:18:14 -0700

    added error logic for user input for display grid

--- src/components/controlButtons.js
+++ src/components/controlButtons.js
@@ -8,10 +8,36 @@ function ControlButtons(props){
     const [msg, setmsg] = useState("Graph size min - max: 2 - 20")
 
     function handleSize(e){
-        setSize(e.target.value)
-        console.log(e.target.value)
-        if (e.target.value < 21){
-            props.update(parseInt(e.target.value))
+        if(msgHandler(e.target.value)){
+            setmsg("")
+            setSize(e.target.value)
+            if (e.target.value < 21){
+                props.update(parseInt(e.target.value))
+            }
+        }
+    }
+
+    function isNumber(input){
+        return !isNaN(parseFloat(input)) && !isNaN(input - 0);
+    }
+    
+    function msgHandler(input){
+        if(isNumber(input)){
+            // more error checks
+            if(input < 2){
+                setmsg("Number too low")
+                return false
+            }
+            else if(input > 20){
+                setmsg("Number too high")
+                return false
+            }
+            
+            return true;
+        }
+        else {
+            setmsg("Not Valid Numeric Input")
+            return false;
         }
     }
 
